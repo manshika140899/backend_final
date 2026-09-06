9baedb05ef30de7c271df334a5a63783f0661716
manshika140899 <manshika140899@gmail.com>
2026-09-06 19:23:23 +0500

    Fix production CORS

--- index.js
+++ index.js
@@ -1,75 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const mongoConnect = require("./config/db");
-// const app = express();
-// app.use(express.json());
-// mongoConnect()
-
-// app.use(cors());
-
-// app.use(express.static("public"));
-
-// const home = require("./routes/home");
-
-// app.use("/home", home);
-
-// const arrival = require("./routes/arrival");
-
-// app.use("/arrival", arrival);
-
-// const selling = require("./routes/selling");
-
-// app.use("/selling", selling);
-
-// const browse = require("./routes/browse");
-
-// app.use("/browse", browse);
-
-// const customer = require("./routes/customer");
-
-// app.use("/customer", customer);
-
-// const related = require("./routes/related");
-
-// app.use("/related", related);
-
-// const faqs = require("./routes/faqs");
-
-// app.use("/faqs", faqs);
-
-// const reviews = require("./routes/reviews");
-// app.use("/reviews", reviews);
-
-// const productInfo = require("./routes/productInfo");
-
-// app.use("/productInfo", productInfo);
-
-// const categoryProducts = require("./routes/categoryProducts");
-
-// app.use("/categoryProducts", categoryProducts);
-
-// const filter = require("./routes/filter");
-
-// app.use("/filter", filter);
-
-// const cart = require("./routes/cart");
-// app.use("/cart", cart);
-
-// const orders = require("./routes/orders"); 
-
-// app.use("/orders", orders);
-
-
-// app.get("/", (req, res) => {
-//   res.send("Backend is running");
-// });
-
-// app.listen(4000, () => {
-//   console.log("Server running on port 4000");
-// });
-
-
-
 const express = require("express");
 const cors = require("cors");
 const mongoConnect = require("./config/db");
@@ -80,8 +8,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      // yahan apna Vercel frontend URL bhi add karna
-      // "https://your-frontend.vercel.app",
+      "https://final-projectfullstack.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
